3dd73c7442628f6ae634b6ff1925dec0e85a0cbd
Guillaume de Bouard <g.debouard@gmail.com>
2024-08-22 19:45:25 +0800

    Part1_Admin_backend

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class AdminController extends Controller
@@ -57,4 +58,40 @@ class AdminController extends Controller
 
         return redirect()->back()->with($notification);
     }
+    public function AdminChangePassword(){
+
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_change_password',compact('profileData'));
+
+    }// End Method   
+    public function AdminUpdatePassword(Request $request){
+
+       $request -> validate([
+        'old_password' => 'required',
+        'new_password' => 'required| confirmed'
+       ]);
+       if(!Hash::check($request->old_password, auth::user()->password )){
+
+        $notification = array(
+            'message' => 'Old password not match',
+            'alert-type' => 'error'
+        );
+
+        return back()->with($notification);
+       }
+        // update the user password
+
+        User::whereId(auth::user()->id)->update([
+            'password' => Hash::make($request->new_password) 
+        ]);
+        $notification = array(
+            'message' => 'Password updated successfully',
+            'alert-type' => 'success'
+        );
+        return back()->with($notification);
+
+    }// End Method   
+
+
 }
